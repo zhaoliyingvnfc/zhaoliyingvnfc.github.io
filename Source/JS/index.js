2d9e34d6b00604af7dba25292ea9270196ccfcbb
zhaoliyingvnfc <nguthemay243@gmail.com>
2017-03-14 22:59:20 +0700

    Add files via upload

---
+++ Source/JS/index.js
@@ -0,0 +1,94 @@
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 4);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 2);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+	
+	$(document).ready(function(){
+	
+	$('a').click(function(){
+		
+		var elem = $(this).closest('.item');
+		
+		$.confirm({
+			'title'		: 'Bạn chắc chắn làm được những điều đó chứ?',
+			'message'	: 'Nếu không làm được thì có thể liên hệ page Tường Thành Vĩnh Cửu để được thông não',
+			'buttons'	: {
+				'Được'	: {
+					'class'	: 'blue',
+					'action': function(){
+						elem.slideUp();
+					}
+				},
+				'Chắc chắn được'	: {
+					'class'	: 'gray',
+					'action': function(){}	// Nothing to do in this case. You can as well omit the action property.
+				}
+			}
+		});
+		
+	});
+	
+});
+
+//Click login home
+$(document).ready(function(){
+	
+	$('body').click(function(){
+				
+		window.location = "https://zhaoliyingvnfc.github.io/home.html";
+		
+	});
+	
+});
