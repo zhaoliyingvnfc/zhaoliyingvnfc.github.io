82cae2fc7b7d5ab71929607d3a8a54408439a98c
zhaoliyingvnfc <nguthemay243@gmail.com>
2017-03-16 20:38:29 +0700

    Add files via upload

--- Source/JS/index.js
+++ Source/JS/index.js
@@ -1,84 +1,32 @@
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+/*
+a Pen by DIACO : twitter.com/Diaco_ml || codepen.io/MAW
+powered by GSAP : http://www.greensock.com/
+*/
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+var total=80,
+    container=document.getElementById('container'),
+    w=window.innerWidth,
+    h=window.innerHeight,
+    Tweens=[],
+    SPs=1;
 
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 4);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 2);
-        }
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-        var that = this;
-        var delta = 200 - Math.random() * 100;
+for (var i=total;i--;){ 
+	var Div=document.createElement('div');
+	TweenLite.set(Div,{attr:{class:'dot'},x:R(w),y:R(h),opacity:0});
+	container.appendChild(Div);	Anim(Div);	Tweens.push(Div);
+};
 
-        if (this.isDeleting) { delta /= 2; }
+function Anim(elm){ 
+	  top: $.dot.offsetTop + $.dot.random(($.dot.eleHeight-50)),	//offsets
+	  left: $.dot.offsetLeft + $.dot.random(($.dot.eleWidth-50)),
+	  opacity: $.dot.opacity($.dot.settings.twinkle)
+  }, (($.dot.random(10) + 5) * 2000),function(){  $.dot.fly(sp) } );
+};
 
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
+for(var i=total;i--;){
+  Tweens[i].Tween.play()};
 
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
-	
-	$(document).ready(function(){
-	
-	$('a').click(function(){
-		
-		var elem = $(this).closest('.item');
-		
-		$.confirm({
-			'title'		: 'Bạn chắc chắn làm được những điều đó chứ?',
-			'message'	: 'Nếu không làm được thì có thể liên hệ page Tường Thành Vĩnh Cửu để được thông não',
-			'buttons'	: {
-				'Được'	: {
-					'class'	: 'blue',
-					'action': function(){
-						elem.slideUp();
-					}
-				},
-				'Chắc chắn được'	: {
-					'class'	: 'gray',
-					'action': function(){}	// Nothing to do in this case. You can as well omit the action property.
-				}
-			}
-		});
-		
-	});
-	
-});
-
+function R(max){return Math.random()*max};
